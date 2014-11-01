11d060b5de8b94c1aa31475d8ab1fe0a824ada64
Chris Nelson <chris@gaslightsoftware.com>
2014-10-31 23:26:36 -0400

    package details

--- package.js
+++ package.js
@@ -1,17 +1,12 @@
 Package.describe({
   name: 'superchris:angular',
   summary: 'AngularJS is a client side javascript MVC framework',
-  version: '1.3.2',
-  git: ' /* Fill me in! */ '
+  version: '1.3.2-build.3509+sha.2a2fd14',
+  git: 'http://github.com/superchris/meteor-angular.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('superchris:angular.js');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('superchris:angular');
-  api.addFiles('superchris:angular-tests.js');
+  api.addFiles('bower-angular/angular.js');
+  api.addFiles('bower-angular/angular-csp.css');
 });
